ac5675844a370050594ae054d9efb0402aaa93a4
ChenChangTing <Danny4156699@gmail.com>
2023-01-05 03:23:09 +0800

    商品功能-基礎實作查詢商品列表

--- src/main/java/com/chendanny/springbootmail/dao/ProductDao.java
+++ src/main/java/com/chendanny/springbootmail/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.chendanny.springbootmail.dao;
 import com.chendanny.springbootmail.dto.ProductRequest;
 import com.chendanny.springbootmail.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
--- src/main/java/com/chendanny/springbootmail/service/ProductService.java
+++ src/main/java/com/chendanny/springbootmail/service/ProductService.java
@@ -4,7 +4,11 @@ package com.chendanny.springbootmail.service;
 import com.chendanny.springbootmail.dto.ProductRequest;
 import com.chendanny.springbootmail.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequset);
 
