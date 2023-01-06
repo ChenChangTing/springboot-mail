d7ec67c41fb9331c9e1bdff1c9b6408b25d9f7df
ChenChangTing <Danny4156699@gmail.com>
2023-01-07 00:51:47 +0800

    商品功能-實作查詢商品列表(分頁+總筆數)

--- src/main/java/com/chendanny/springbootmail/controller/ProductController.java
+++ src/main/java/com/chendanny/springbootmail/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.chendanny.springbootmail.dto.ProductQueryParams;
 import com.chendanny.springbootmail.dto.ProductRequest;
 import com.chendanny.springbootmail.model.Product;
 import com.chendanny.springbootmail.service.ProductService;
+import com.chendanny.springbootmail.util.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +25,7 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts(
+    public ResponseEntity<Page<Product>> getProducts(
             //查詢條件
             @RequestParam(required = false) ProductCategory category,
             @RequestParam(required = false) String search,
@@ -46,9 +47,20 @@ public class ProductController {
         productQueryParams.setLimit(limit);
         productQueryParams.setOoffset(offset);
 
+        //取得product list
         List<Product> productList= productService.getProducts(productQueryParams);
 
-        return ResponseEntity.status(HttpStatus.OK).body(productList);
+        //取得product總數
+        Integer total=productService.countProduct(productQueryParams);
+
+        //分頁
+        Page<Product> page=new Page<>();
+        page.setLimit(limit);
+        page.setOffset(offset);
+        page.setTotal(total);
+        page.setResults(productList);
+
+        return ResponseEntity.status(HttpStatus.OK).body(page);
     }
 
 
